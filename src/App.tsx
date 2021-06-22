85d3f1a10eac5ecd7ef8b8ca67791af19a33f7a2
Vinicius Resende <alive2k@programmer.net>
2021-06-22 12:13:35 -0300

    Notion Next: Firenase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
-}
+import {Route, BrowserRouter} from 'react-router-dom'
 
-export default App;
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+  )
+}
